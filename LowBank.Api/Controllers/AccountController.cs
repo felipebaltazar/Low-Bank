39db374dcc2c7fc13f75f971851d0804cb8a8769
Felipe Baltazar <felipe.dasilvabaltazar@gmail.com>
2023-02-08 22:24:52 -0300

    Criado projeto api

---
+++ LowBank.Api/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using LowBank.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LowBank.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountController : ControllerBase
+    {
+        [HttpGet(Name = "GetAccounts")]
+        public IEnumerable<Account> Get()
+        {
+            var listaDeContas = new List<Account>();
+
+            listaDeContas.Add(new Account(123, 500, 100));
+
+            listaDeContas.Add(new Account(999, 235.65m, 75));
+
+            listaDeContas.Add(new Account(564, 5, 0));
+
+            return listaDeContas;
+        }
+    }
+}
