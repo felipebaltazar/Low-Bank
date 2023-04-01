2084c394a7fc6cc41db1a933fb1bc53b85b9a3d6
Felipe Baltazar <felipe.dasilvabaltazar@gmail.com>
2023-03-31 22:00:34 -0300

    Criado projeto mobile

---
+++ LowBank-Mobile/AppShell.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace LowBank_Mobile;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+
+        Routing.RegisterRoute("registrationpage", typeof(RegistrationPage));
+    }
+}
---
+++ LowBank-Mobile/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace LowBank_Mobile;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void LoginBtn_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private async void SignBtn_Clicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("registrationpage");
+    }
+}
+
